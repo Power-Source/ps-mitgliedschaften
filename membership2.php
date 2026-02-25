5d64b197ab09ffec5231c5e96dcc9cab8a2d13fc
DerN3rd <dern3rd@outlook.at>
2026-02-25 18:28:23 +0100

    Aktualisiere Versionsnummer auf 1.0.1 und verbessere die Kompatibilität mit WordPress und ClassicPress

--- membership2.php
+++ membership2.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: PS Mitgliedschaften
  * Plugin URI:  https://cp-psource.github.io/mitgliedschaften-pro/
- * Version:     1.0.0
+ * Version:     1.0.1
  * Description: Das leistungsstärkste, benutzerfreundlichste und flexibelste Mitgliedschafts-Plugin für ClassicPress-Seiten.
  * Requires at least: 4.6
  * Tested up to: 6.8.1
@@ -43,43 +43,6 @@
  *
  * @since  1.0.0
  */
-// PS Update Manager - Hinweis wenn nicht installiert
-add_action( 'admin_notices', function() {
-    // Prüfe ob Update Manager aktiv ist
-    if ( ! function_exists( 'ps_register_product' ) && current_user_can( 'install_plugins' ) ) {
-        $screen = get_current_screen();
-        if ( $screen && in_array( $screen->id, array( 'plugins', 'plugins-network' ) ) ) {
-            // Prüfe ob bereits installiert aber inaktiv
-            $plugin_file = 'ps-update-manager/ps-update-manager.php';
-            $all_plugins = get_plugins();
-            $is_installed = isset( $all_plugins[ $plugin_file ] );
-            
-            echo '<div class="notice notice-warning is-dismissible"><p>';
-            echo '<strong>PS Chat:</strong> ';
-            
-            if ( $is_installed ) {
-                // Installiert aber inaktiv - Aktivierungs-Link
-                $activate_url = wp_nonce_url(
-                    admin_url( 'plugins.php?action=activate&plugin=' . urlencode( $plugin_file ) ),
-                    'activate-plugin_' . $plugin_file
-                );
-                echo sprintf(
-                    __( 'Aktiviere den <a href="%s">PS Update Manager</a> für automatische Updates von GitHub.', 'psource-chat' ),
-                    esc_url( $activate_url )
-                );
-            } else {
-                // Nicht installiert - Download-Link
-                echo sprintf(
-                    __( 'Installiere den <a href="%s" target="_blank">PS Update Manager</a> für automatische Updates aller PSource Plugins & Themes.', 'psource-chat' ),
-                    'https://github.com/Power-Source/ps-update-manager/releases/latest'
-                );
-            }
-            
-            echo '</p></div>';
-        }
-    }
-});
-
 
 
 function membership2_pro_init_app() {
@@ -105,7 +68,7 @@ function membership2_pro_init_app() {
 	 *
 	 * @since  1.0.0
 	 */
-	define( 'MS_PLUGIN_VERSION', '1.0.0' );
+	define( 'MS_PLUGIN_VERSION', '1.0.1' );
 
 	/**
 	 * Free or pro plugin?
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: PSOURCE
 Tags: classicpress-plugin, classicpress, membership, multisite
 Requires at least: 4.9
-Tested up to: 6.8.1
-Stable tag: 1.0.0
+Tested up to: WordPress 6.8.1, ClassicPress 2.6.0
+Stable tag: 1.0.1
 Requires PHP: 7.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -33,6 +33,10 @@ Use the opportunity to do so on GitHub or send your files to: webmaster@n3rds.wo
 
 
 == ChangeLog ==
+
+= 1.0.1 =
+
+* Security Fixes
 
 = 1.0.0 =
 
